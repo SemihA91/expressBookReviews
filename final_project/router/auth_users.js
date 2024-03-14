76ebf952f2d075d3a393d86ba23094bf22d4a1db
Semih Altinpinar <semihaltinpinar@gmail.com>
2024-03-14 13:13:30 -0400

    completed tasks

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const jwt = require('jsonwebtoken');
 let books = require("./booksdb.js");
+const session = require('express-session');
 const regd_users = express.Router();
 
 let users = [];
@@ -10,20 +11,66 @@ const isValid = (username)=>{ //returns boolean
 }
 
 const authenticatedUser = (username,password)=>{ //returns boolean
-//write code to check if username and password match the one we have in records.
+    let validUsers = users.filter((user) => {
+        return (user.username === username && user.password === password)
+    });
+    if (validUsers.length > 0) {
+        return true;
+    } else {
+        return false;
+    }
 }
 
 //only registered users can login
 regd_users.post("/login", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const username = req.body.username;
+  const password = req.body.password;
+
+  if (!username || !password) { //no username or password provided
+    return res.status(404).json({message:"No username or password provided. Error logging in."});
+  }
+
+  if (authenticatedUser(username, password)) {
+    let accessToken = jwt.sign({
+        data:password
+    }, 'access', {expiresIn: 60*60});
+
+    req.session.authorization = {
+        accessToken, username
+    }
+
+    return res.status(200).json({message:"User signed in."});
+  } else {
+    return res.status(208).json({message: "Invalid credentials, check again."});
+  }
+  
 });
 
 // Add a book review
 regd_users.put("/auth/review/:isbn", (req, res) => {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    let isbn= req.params.isbn;
+    let reviewContent = req.body.review;
+    let username = req.session.authorization.username;
+    let book = books[isbn];
+    
+    book.reviews[username] = reviewContent
+    return res.status(200).json({message:"Review added successfully"});
 });
+
+regd_users.delete("/auth/review/:isbn", (req,res) => {
+    let isbn= req.params.isbn;
+    let username = req.session.authorization.username;
+    let book = books[isbn];
+    if (username in book.reviews){
+        delete book.reviews[username];
+        return res.status(200).json({message:"Review deleted successfully"});
+    } else {
+        return res.status(404).json({message: "Unable to delete review."});
+    }
+
+
+})
 
 module.exports.authenticated = regd_users;
 module.exports.isValid = isValid;
--- final_project/router/general.js
+++ final_project/router/general.js
@@ -4,40 +4,106 @@ let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
 const public_users = express.Router();
 
-
 public_users.post("/register", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
-});
+    const username = req.body.username;
+    const password = req.body.password;
+
+    if (username && password) {
+        if(!doesExist(username)) {
+            users.push({"username":username, "password":password});
+            return res.status(200).json({message:"User successfully registered. You may now log in."});
+        } else {
+            return res.status(404).json({message:"User already exists. Cannot create duplicate users."});
+        }
+    }
+    return res.status(404).json({message:"Unable to register new user."})
+})
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
+    let promise1 = new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve()
+        }, 2000)
+    })
+    promise1.then(() => {
+        return res.send(JSON.stringify(books, null, 4));
+    })
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+ 
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
- });
+  let promise1 = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        resolve()
+    }, 2000)
+    })
+    promise1.then(() => {
+        let isbn = req.params.isbn;
+    return res.send(JSON.stringify(books[isbn]))
+    });
+})
+  
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    //Write your code here
+    let promise1 = new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve()
+        }, 2000)
+        })
+        promise1.then(() => {
+            let authorName = req.params.author;
+
+            for (var key in books) {
+                if (books[key].author == authorName) {
+                    return res.send(JSON.stringify(books[key], null, 4));
+                }
+            }
+            return res.send("Unable to find book with given author.")
+        });
 });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    let promise1 = new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve()
+        }, 2000)
+        })
+        promise1.then(() => {
+            let titleName = req.params.title;
+            for (var key in books) {
+                if (books[key].title == titleName) {
+                    return res.send(JSON.stringify(books[key], null, 4));
+                }
+            }
+            return res.send("Unable to find book with given title.")
+    });
 });
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  let isbn = req.params.isbn;
+  return res.send(JSON.stringify(books[isbn].reviews))
+  
 });
+
+const doesExist = (username)=>{
+    let userswithsamename = users.filter((user)=>{
+      return user.username === username
+    });
+    if(userswithsamename.length > 0){
+      return true;
+    } else {
+      return false;
+    }
+  }
+
 
 module.exports.general = public_users;
